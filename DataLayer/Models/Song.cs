2f67b3a54cae4c43a7adb3cf56ccb1f70324d3bf
Steven Kent <stevenpkent@icloud.com>
2016-02-04 20:39:22 -0500

    added Song.cs

---
+++ DataLayer/Models/Song.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Models
+{
+    public class Song
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public virtual Band Band { get; set; } //song is by a band
+        public virtual Album Album { get; set; } //song is on an album
+    }
+}
